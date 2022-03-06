bd2e1370f09643ec53e2266633c5abd33393ea9a
Lukas Paluch <fluktuid@users.noreply.github.com>
2022-03-06 22:37:41 +0100

    fix src

--- src/barista/main.go
+++ src/barista/main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"time"
-
-	log "github.com/sirupsen/logrus"
 
 	"github.com/fluktuid/coffee-shop/src/dto"
 	"github.com/fluktuid/coffee-shop/src/util"
@@ -35,29 +31,4 @@ func main() {
 	}()
 
 	select {}
-	util.LoadEnv(config)
-
-	http.HandleFunc("/order", func(w http.ResponseWriter, r *http.Request) {
-		log.Info("ordered some coffee")
-		fmt.Fprintf(w, "Hello!\n")
-		keys, ok := r.URL.Query()["type"]
-		if !ok {
-			keys = []string{"coffee"}
-		}
-
-		// 'brew' coffee
-		for _, e := range keys {
-			brew(config, e)
-			fmt.Fprintf(w, "Here, your %s\n", e)
-		}
-	})
-
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func brew(c dto.Config, _type string) string {
-	c.Machine
 }
